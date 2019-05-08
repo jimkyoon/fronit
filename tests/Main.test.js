8e94f95c91a577936cdb21ffa35976ed7831d129
Jim Yoon <jimyoon90@gmail.com>
2019-05-08 02:04:32 -0700

    got rid of test for ignoreTest from previous commit, changed travis config

--- tests/Main.test.js
+++
@@ -1,7 +0,0 @@
-const hello = require('../ignoreTest');
-
-describe('Testing main file', () => {
-  test('should print out "Hello World!"', () => {
-    expect(hello()).toMatch('Hello World!');
-  });
-});
