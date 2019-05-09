33c5e90f17bf598f6b5781aaf4ad187fc34650c1
Jim Yoon <jimyoon90@gmail.com>
2019-05-08 17:53:10 -0700

    continuing to work on the main process, added in commander options for react, vue and angular

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 
-// grab all inputs in an array from process.argv
-const [,, ...args] = process.argv;
+const program = require('commander');
+const chalk = require('chalk');
 
-// grab the first value
-const firstArg = args[0];
+program
+  .option('r, react')
+  .option('R, React')
+  .option('v, vue')
+  .option('V, Vue')
+  .option('a, angular')
+  .option('A, Angular');
+
+program.parse(process.argv);
+
+// React
+if (program.react === true || program.React === true) {
+  console.log('Creating React component!');
+}
+// Vue
+if (program.vue === true || program.Vue === true) console.log(chalk.hex('#42b883')('Vue functionality coming soon!'));
+if (program.angular === true || program.Angular === true) console.log(chalk.hex('#b52e31')('Angular functionality coming soon!'));
