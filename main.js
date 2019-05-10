497faa143472cf2a080c0897bb28b4c799fea907
Jim Yoon <jimyoon90@gmail.com>
2019-05-09 22:41:39 -0700

    reduced options to keep it simiple, if checks based on commander program args, just console.logs with chalk for now

--- main.js
+++ main.js
@@ -3,20 +3,37 @@
 const program = require('commander');
 const chalk = require('chalk');
 
+// library colors
+const reactColor = '#00d8ff';
+const vueColor = '#42b883';
+const angularColor = '#b52e31';
+
+// set up options/flags
 program
-  .option('r, react')
-  .option('R, React')
-  .option('v, vue')
-  .option('V, Vue')
-  .option('a, angular')
-  .option('A, Angular');
+  .option('R, React [type]', 'Create a new React Component')
+  .option('V, Vue [type]', 'Create a new Vue Component')
+  .option('A, Angular [type]', 'Create a new Angular Component');
 
 program.parse(process.argv);
 
+// redirect process to respective files
 // React
-if (program.react === true || program.React === true) {
-  console.log('Creating React component!');
+if (program.React === true) {
+  console.log('Please give your component a name!');
+} else if (typeof program.React === 'string') {
+  const name = program.React;
+  console.log(chalk.bold.hex(reactColor)('Creating React component!'));
+  console.log(chalk.bold.hex(reactColor)(`${name}`));
 }
 // Vue
-if (program.vue === true || program.Vue === true) console.log(chalk.hex('#42b883')('Vue functionality coming soon!'));
-if (program.angular === true || program.Angular === true) console.log(chalk.hex('#b52e31')('Angular functionality coming soon!'));
+if (program.Vue === true) {
+  console.log(chalk.bgRed('ATTENTION!'), chalk.bold.hex(vueColor)('Vue functionality coming soon!'));
+} else if (typeof program.Vue === 'string') {
+  console.log(chalk.bgRed('ATTENTION!'), chalk.bold.hex(vueColor)('Vue functionality coming soon!'));
+}
+// Angular
+if (program.Angular === true) {
+  console.log(chalk.bgRed('ATTENTION!'), chalk.bold.hex(angularColor)('Angular functionality coming soon!'));
+} else if (typeof program.Angular === 'string') {
+  console.log(chalk.bgRed('ATTENTION!'), chalk.bold.hex(angularColor)('Angular functionality coming soon!'));
+}
