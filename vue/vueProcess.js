6fc22f70294f065cbb163cac9b711a723eda14b2
Jim Yoon <jimyoon90@gmail.com>
2019-05-31 12:01:26 -0700

    starting on vue process

--- vue/vueProcess.js
+++ vue/vueProcess.js
@@ -1,23 +1,44 @@
 const program = require('commander');
 const chalk = require('chalk');
 
+// utility
+const pascalFormat = require('../utils/formatPascal');
+const formatBlankLines = require('../utils/formatBlankLines');
+const createFile = require('../utils/createFile');
+
 const vueColor = '#42b883';
 
 /**
  * Process args through commander for Vue components and send them to template generators
  * @param {Array} args - process.argv, array of inputs
  * @param {String} pwd - process.cwd, string for current directory path
- * @returns {File} a Angular component in .vue or .js
+ * @returns {File} a Vue component in .vue or .js
  */
 
-const vueProcess = (args, pwd) => {
+const vueProcess = async (args, pwd) => {
   program
     .option('V, Vue [type]', 'Create a new Vue component');
     // more options coming soon
 
   program.parse(args);
 
-  if (typeof program.Vue === 'string') console.log(chalk.bold.hex(vueColor)('Working!'), `${program.Vue}`);
+  const compData = {
+    name: '',
+    fileType: '',
+    extension: '',
+  };
+
+  // UNCOMMENT THIS SECTION AFTER FINISHING VUE TEMPLATES
+  // // create file after getting template
+  // const file = createFile(pwd, compData.name, compData.extension, template);
+
+  // const finish = await file;
+
+  // if (finish) {
+  //   console.log(chalk.bold.hex(vueColor)(`Finished building ${compData.name} component!`));
+  // } else {
+  //   console.log('something happened!');
+  // }
 
   console.log(chalk.bgRed('ATTENTION!'), chalk.bold.hex(vueColor)('Vue functionality coming soon!'));
 };
